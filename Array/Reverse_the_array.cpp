ef300e52a69a8f21f0dd98aa89c7a6023854af62
Shreya Ghosh <sgshreya024@gmail.com>
2021-11-17 06:28:33 +0530

    Reverse the Array Uploaded

--- Array/Reverse_the_array.cpp
+++ Array/Reverse_the_array.cpp
@@ -5,3 +5,31 @@ Output : arr[] = {3, 2, 1}
 
 Input :  arr[] = {4, 5, 1, 2}
 Output : arr[] = {2, 1, 5, 4}*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    int n;
+    cin >> n;
+    int arr[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    for (int i = 0; i < n / 2; i++)
+    {
+        int temp = arr[n - 1 - i];
+        arr[n - 1 - i] = arr[i];
+        arr[i] = temp;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
