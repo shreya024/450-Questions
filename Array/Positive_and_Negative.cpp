6c60f1dc381a9229e11bbff8227b86ea8fe66eb4
Shreya Ghosh <sgshreya024@gmail.com>
2021-12-07 06:50:38 +0530

    Positive and Negative Numbers Added

--- Array/Positive_and_Negative.cpp
+++ Array/Positive_and_Negative.cpp
@@ -1,1 +1,37 @@
 //Move all negative numbers to beginning and positive to end with constant extra space
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    int n;
+    cin >> n;
+    int arr[n];
+
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    int j = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] < 0)
+        {
+            if (i != j)
+            {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+            j++;
+        }
+    }
+
+    cout << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
