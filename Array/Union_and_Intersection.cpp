a661181e46ccb9d0eaae911c364dc69ab219f7a4
Shreya Ghosh <sgshreya024@gmail.com>
2021-12-29 11:04:22 +0530

    EKO SPOJ added

---
+++ Array/Union_and_Intersection.cpp
@@ -0,0 +1,4 @@
+/*Given two arrays a[] and b[] of size n and m respectively. The task is to find union between these two arrays.
+
+Union of the two arrays can be defined as the set containing distinct elements from both the arrays. 
+If there are repetitions, then only one occurrence of element should be printed in the union.*/
---
+++ Binary Search/EKO_SPOJ.cpp
@@ -0,0 +1,68 @@
+/*Lumberjack Mirko needs to chop down M metres of wood. It is an easy job for him since he has a nifty new woodcutting machine that can take down forests like wildfire. However, Mirko is only allowed to cut a single row of trees.
+
+Mirko‟s machine works as follows: Mirko sets a height parameter H (in metres), and the machine raises a giant sawblade to that height and cuts off all tree parts higher than H (of course, trees not higher than H meters remain intact). Mirko then takes the parts that were cut off. For example, if the tree row contains trees with heights of 20, 15, 10, and 17 metres, and Mirko raises his sawblade to 15 metres, the remaining tree heights after cutting will be 15, 15, 10, and 15 metres, respectively, while Mirko will take 5 metres off the first tree and 2 metres off the fourth tree (7 metres of wood in total).
+
+Mirko is ecologically minded, so he doesn‟t want to cut off more wood than necessary. That‟s why he wants to set his sawblade as high as possible. Help Mirko find the maximum integer height of the sawblade that still allows him to cut off at least M metres of wood.
+
+Input
+The first line of input contains two space-separated positive integers, N (the number of trees, 1 ≤ N ≤ 1 000 000) and M (Mirko‟s required wood amount, 1 ≤ M ≤ 2 000 000 000).
+
+The second line of input contains N space-separated positive integers less than 1 000 000 000, the heights of each tree (in metres). The sum of all heights will exceed M, thus Mirko will always be able to obtain the required amount of wood.
+
+Output
+The first and only line of output must contain the required height setting.
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isPossible(vector<int> &v, int N, int M, int mid)
+{
+    long long int sum = 0;
+    for (long long int i = 0; i < N; i++)
+    {
+        if (mid > v[i])
+        {
+            sum += v[i] - mid;
+        }
+        if (sum >= M)
+            return true;
+    }
+    return false;
+}
+
+int Cutting(vector<int> &v, int N, int M)
+{
+    long long int s = 0;
+    long long int e = v[N - 1];
+    long long int mid = s + (e - s) / 2;
+    long long int ans = -1;
+    while (s <= e)
+    {
+        if (isPossible(v, N, M, mid))
+        {
+            ans = mid;
+            s = mid + 1;
+        }
+        else
+            e = mid - 1;
+        mid = s + (e - s) / 2;
+    }
+    return ans;
+}
+
+int main()
+{
+    long long int N, M;
+    cin >> N >> M;
+    vector<int> v;
+    for (long long int i = 0; i < N; i++)
+    {
+        cin >> v[i];
+    }
+    sort(v.begin(), v.end());
+    long long int result = Cutting(v, N, M);
+    cout << result;
+    return 0;
+}
